ccc349e999ded2c247ca0f8939a7db9a2d8b10d6
Leonardo <leomds94@gmail.com>
2019-07-20 13:19:39 +0200

    more design

--- src/views/OfferDetail.vue
+++ src/views/OfferDetail.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="hello">
     <router-link :to="{ name: 'offers'}">
-      <a style='display:flex;padding-left:25%'>Back</a>
+      <a style='display:flex;padding-left:15%'>Back</a>
     </router-link>
 
+    <b-card class='main-card'>
     <h2>{{ result.car.make }} {{ result.car.model }} {{ result.car.version }}</h2>
 
     <b-carousel
@@ -14,9 +15,8 @@
     >
       <b-carousel-slide v-for="image in result.images" :key='image' class="mySlides" :img-src="image.src"></b-carousel-slide>
     </b-carousel>
-
-    <form>
-      <fieldset class="field-tight">
+    <b-card-group deck>
+    <b-card header="Details">
         <legend>Your subscription:</legend>
         <div>
           <ul>
@@ -27,24 +27,29 @@
             </li>
           </ul>
         </div>
-      </fieldset>
+    </b-card>
 
-      <fieldset style="width: 35%; margin: auto">
-        <legend>Details:</legend>
-        <b-row>
-          <b-col lg='6' v-for="detail in result.car.equipmentDetails" :key='detail'>
-            {{ detail }}
-          </b-col>
-          <b-col lg='6'>
-            {{ result.car.kw }} kW / {{ result.car.ps }} hp
-          </b-col>
-          <b-col lg='6'>{{ result.car.gearingType }}</b-col>
-          <b-col lg='6'>{{ result.car.drive }}</b-col>
-          <b-col lg='6'>{{ result.car.fueltype }}</b-col>
-          <b-col lg='6'>{{ result.car.doors }} doors</b-col>
-        </b-row>
-      </fieldset>
-    </form>
+      <!-- <b-card-group deck> -->
+        <b-card header="Details">
+          <b-row>
+            <b-col>
+          <b-list-group>
+            <b-list-group-item v-for="detail in result.car.equipmentDetails" :key='detail'>{{ detail }}</b-list-group-item>
+          </b-list-group>
+            </b-col>
+            <b-col>
+          <b-list-group>
+            <b-list-group-item>{{ result.car.kw }} kW / {{ result.car.ps }} hp</b-list-group-item>
+            <b-list-group-item>{{ result.car.gearingType }}</b-list-group-item>
+            <b-list-group-item>{{ result.car.drive }}</b-list-group-item>
+            <b-list-group-item>{{ result.car.fueltype }}</b-list-group-item>
+            <b-list-group-item>{{ result.car.doors }} doors</b-list-group-item>
+          </b-list-group>
+            </b-col>
+          </b-row>
+        </b-card>
+      </b-card-group>
+    </b-card>
   </div>
 </template>
 
@@ -89,14 +94,32 @@ export default {
 .carousel.pointer-event {
   width: 50%;
   margin: auto;
+  padding-bottom: 20px;
 }
 
-.carousel-control-prev .carousel-control-next {
+.carousel-control-prev {
   background-image: linear-gradient(to left, black , white) !important;
 }
 
 .carousel-control-prev {
   background-image: linear-gradient(to right, black , white) !important;
+}
+
+.main-card {
+  width: 70%;
+  margin: auto;
+}
+
+.carousel-control-prev-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E");
+}
+
+.carousel-control-next-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%23000' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E");
+}
+
+.card-deck .card {
+  display: inline-table;
 }
 
 </style>
